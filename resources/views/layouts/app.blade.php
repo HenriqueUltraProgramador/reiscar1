bfd1f193c900927febbfdb0884e5ba2d51dbab1d
Alexandreabreu46 <Alexandreabreu46@hotmail.com>
2022-04-15 00:43:58 +0100

    fix route language switcher

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,14 +73,6 @@
             </div>
         </nav>
 
-
-
-
-
-
-
-
-
         <div id="app">
             <nav class="navbar navbar-expand-md navbar-light  shadow-sm">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
